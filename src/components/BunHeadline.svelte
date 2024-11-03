9bd0d2c9467581a5e0c930a214708f312922c627
Roger Pence <roger.pence@gmail.com>
2024-11-02 23:36:40 -0500

    initial commit

---
+++ src/components/BunHeadline.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    // Your TypeScript code here
+</script>
+
+<h2>This is the headline</h2>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import BunHeadline from '../components/BunHeadline.svelte';
+
+    onMount(() => { 
+        console.log('hello')
+    });
+</script>
+
+<BunHeadline />
